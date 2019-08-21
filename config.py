dd0431418ee9c8757ae0d58818e0577b957be909
n0na7773 <37002484+n0na7773@users.noreply.github.com>
2019-08-21 11:00:40 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+token = '689601090:AAHI0v_DTmhhdEVBXn73USHnOymwYpyvLHc'
+database_name = 'music.db'  # Файл с базой данных
+shelve_name = 'shelve.db'  # Файл с хранилищем
+anton_id = '599495956'
+my_id = '426672281'
